0b9b8a786433e7de44ac49edbbfa7c87a1ae34de
Mark Mbugua <mbuguamark@gmail.com>
2019-03-27 03:40:23 +0300

    create and update forms

--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.views.decorators.http import require_POST
 
+from .forms import TodoForm
 from .models import Todo
 
 # Create your views here.
 def index(request):
     todo_list = Todo.objects.order_by('id')
-    context = {'todo_list': todo_list}
+    form = TodoForm()
+    context = {'todo_list': todo_list, 'form': form}
     return render(request, 'todoapp/index.html', context)
+
+@require_POST
+def addTodo(request):
+    form = TodoForm(request.POST)
+    new_todo = Todo(text=request.POST['text'])
+    new_todo.save()
+    return redirect('index')
+
+def completeTodo(request, todo_id):
+    todo = Todo.objects.get(pk=todo_id)
+    todo.complete = True
+    todo.save()
+
+    return redirect('index')
+
